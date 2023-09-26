020e4f2ee14556eda13138e18b53a06011eb5790
Philip Toan Nguyen <Philip2an@hotmail.com>
2023-09-26 15:19:10 +0200

    Lagt til topbar med logo.

--- app/src/main/java/com/example/mob3000oblig/MainActivity.kt
+++ app/src/main/java/com/example/mob3000oblig/MainActivity.kt
@@ -3,12 +3,23 @@ package com.example.mob3000oblig
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mob3000oblig.ui.theme.Mob3000ObligTheme
 
@@ -22,25 +33,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SkiltsokerenApp(modifier: Modifier = Modifier) {
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = {
+                Image(painter = painterResource(id = R.drawable.skiltskern ), contentDescription = "skiltskern")
+
+
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = colorResource(R.color.purple_700),
+                titleContentColor = colorResource(R.color.black),
+            ),
+        )
+    },
+        content = {it -> Column(modifier = modifier.padding(it)) {
+            Text(text = "yo")
+        }})
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Mob3000ObligTheme {
-        Greeting("Android")
+    SkiltsokerenApp()
     }
 }
