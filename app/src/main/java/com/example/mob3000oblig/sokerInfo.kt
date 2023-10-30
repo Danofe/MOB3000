b9ddc9e911edd6a2d97d1022bfddbf15dfcf6ee4
Philip Toan Nguyen <Philip2an@hotmail.com>
2023-10-30 15:24:30 +0100

    Gjort noe forandringer i forhold til API

--- app/src/main/java/com/example/mob3000oblig/sokerInfo.kt
+++ app/src/main/java/com/example/mob3000oblig/sokerInfo.kt
@@ -1,5 +1,6 @@
 package com.example.mob3000oblig
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,6 +13,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -19,12 +24,24 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+import com.example.mob3000oblig.DataApi.api
+import com.example.mob3000oblig.DataModeller.KjoretoyDataListe
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
 class sokerInfo {
 
     //Test
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
+
+
     fun SkiltInfo(name: String?,modifier: Modifier = Modifier) {
+
+
+
         Scaffold(topBar = {
             CenterAlignedTopAppBar(
                 title = {
@@ -48,7 +65,6 @@ class sokerInfo {
                         .padding(top = 40.dp))
 
 
-
                     Column(
                         modifier = modifier
                             .align(Alignment.CenterStart)
@@ -57,14 +73,32 @@ class sokerInfo {
                         horizontalAlignment = Alignment.Start,
                     ) {
 
-                        Text(text = "Mål: ")
-                        Text(text = "Vekt: ")
-                        Text(text = "EU-Kontroll: ")
-                        Text(text = "Registreringsdata: ")
-                        Text(text = "Utslipp: ")
-                        Text(text = "Dekk: ")
-                        Text(text = "Felg: ")
-                        Text(text = "Etc: ")
+                        var responseData by remember { mutableStateOf<String>("") }
+                        var bilinfo by remember { mutableStateOf<String>("") }
+                        var data by remember { mutableStateOf<String>("") }
+                        api.getKjoretoyDataListe().enqueue(object : Callback<KjoretoyDataListe> {
+                            override fun onResponse(
+                                call: Call<KjoretoyDataListe>,
+                                response: Response<KjoretoyDataListe>
+                            ) {
+
+                                if(response.isSuccessful) {
+                                    var data = response.body()
+                                    var informasjon = data?.kjoretoydataListe?.get(0)?.kjoretoyId?.understellsnummer + "\n" +
+                                                      data?.kjoretoydataListe?.get(0)?.godkjenning?.tekniskGodkjenning?.tekniskeData?.dimensjoner + "\n"
+
+                                    Log.d("ResponseCheck", "Response: $data")
+                                    bilinfo = informasjon
+                                }
+                            }
+
+                            override fun onFailure(call: Call<KjoretoyDataListe>, t: Throwable) {
+                                Log.i("ResponseCheck", "Response: ${t.message}")
+                            }
+                        })
+
+                        Text(text = bilinfo)
+
                     }
                 }
             }
